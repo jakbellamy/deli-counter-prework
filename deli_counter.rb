656c4dd06c9a52516c3e7e3e0d78a6b568845439
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-06 19:26:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli = [])
+  if katz_deli == []
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |name, position|
+      current_line << " #{position}. #{name}"
+    end
+    puts current_line
+  end
+end
+
+
+
+   
